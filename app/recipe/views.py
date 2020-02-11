f7e9cfd0ce6a459b541fda4123b091edfc7ddbfe
Ganesh Chincholkar <gnesh.pc@gmail.com>
2020-02-11 12:11:23 +0530

    Added create tag feature.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,7 +6,9 @@ from core.models import Tag
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """
     Manage tags in the database
     """
@@ -19,3 +21,7 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """ Return object for the current authnticated user only  """
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """ Create a new tag """
+        serializer.save(user=self.request.user)
