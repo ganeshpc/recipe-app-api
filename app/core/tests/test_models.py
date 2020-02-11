701da8eb7a9ae7e359acc60bdd080f2f5a78eefc
Ganesh Chincholkar <gnesh.pc@gmail.com>
2020-02-11 12:01:35 +0530

    Added end point for list tags.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='test@gmail.com', password='password'):
+    """ Create sample user """
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
 
@@ -44,3 +51,12 @@ class ModelTests(TestCase):
         """ Test creating user with no email raise error """
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'password')
+
+    def test_tag_str(self):
+        """ Test the tag string representation """
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
